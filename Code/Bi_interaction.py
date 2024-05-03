6b965149d94e1671dc32473aef04a2d470802df4
wuwenqing <108659065+njust-winchy@users.noreply.github.com>
2024-05-03 17:52:14 +0800

    Update Bi_interaction.py

--- Code/Bi_interaction.py
+++ Code/Bi_interaction.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from transformers import *
 import math
 import torch.nn.functional as F
-
+#Global variables
 RANDOM_SEED = 400
 np.random.seed(RANDOM_SEED)
 torch.manual_seed(RANDOM_SEED)
@@ -45,7 +45,7 @@ class SparseAttention(nn.Module):
         self.head_dim = input_dim // num_heads
         self.sparse_dim = sparse_dim
 
-       
+        #Attention weight parameter
         self.q_linear = nn.Linear(input_dim, input_dim)
         self.kv_linear = nn.Linear(input_dim, 2*input_dim)
         self.fc_out = nn.Linear(input_dim, input_dim)
@@ -53,18 +53,18 @@ class SparseAttention(nn.Module):
     def forward(self, x, y):
         b, t, _ = x.size()
         m, n, _ = y.size()
-        
+        #Calculate attention weights
         q = self.q_linear(x).view(b, t, 1, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
         kv = self.kv_linear(y).view(m, n, 2, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
         k = kv[0]
         v = kv[1]
         attn_weights = torch.matmul(q[0], k.transpose(-2, -1)) / math.sqrt(self.head_dim)
-        
+        #Selecting Partial Positions for Attention
         attn_weights = F.softmax(attn_weights[:, :, :self.sparse_dim, :self.sparse_dim], dim=-1)
-        
+        #Attention weighted summation
         attn_output = torch.matmul(v, attn_weights)
         attn_output = attn_output.permute(0, 2, 1, 3).contiguous().view(-1, n, 768)
-
+        #Linear transformation and residual connection
         
         output = self.fc_out(attn_output) + y
         return output
@@ -76,19 +76,19 @@ class FeedForwardNetwork(nn.Module):
         super(FeedForwardNetwork, self).__init__()
         layers = []
 
-        
+        #Input layer to hidden layer
         layers.append(nn.Linear(input_dim, hidden_dim))
         layers.append(activation)
 
-        
+        #Hidden layer to hidden layer
         for _ in range(num_layers - 1):
             layers.append(nn.Linear(hidden_dim, hidden_dim))
             layers.append(activation)
 
-        
+        #Hidden layer to output layer
         layers.append(nn.Linear(hidden_dim, output_dim))
 
-       
+        #Building a feedforward neural network
         self.ffn = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -102,25 +102,25 @@ class SelfAttention(nn.Module):
         self.num_heads = num_heads
         self.head_dim = input_dim // num_heads
 
-        
+        #Attention weight parameter
         self.qkv_linear = nn.Linear(input_dim, 3 * input_dim)
 
     def forward(self, x):
         b, t, _ = x.size()
 
-       
+        #Calculate attention weights
         qkv = self.qkv_linear(x).view(b, t, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]
-
+        
         attn_weights = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.head_dim)
 
-        
+        #Using softmax to calculate attention weights
         attn_weights = F.softmax(attn_weights, dim=-1)
 
-        
+        #Attention weighted summation
         attn_output = torch.matmul(attn_weights, v)
         attn_output = attn_output.permute(0, 2, 1, 3).contiguous().view(b, t, -1)
-
+        
         return attn_output
 
 class SelfAttentionReduction(torch.nn.Module):
@@ -130,12 +130,12 @@ class SelfAttentionReduction(torch.nn.Module):
         self.attention_weights = torch.nn.Linear(input_dim, 1)
 
     def forward(self, x):
-       
+        #Calculate attention weights
         attn_weights = F.softmax(self.attention_weights(x), dim=1)
 
      
         reduced_representation = torch.sum(attn_weights * x, dim=1)
-
+        #Weighted average
         return reduced_representation
 
 class FullyConnectedLayer(nn.Module):
@@ -144,5 +144,6 @@ class FullyConnectedLayer(nn.Module):
         self.fc = nn.Linear(input_size, output_size)
 
     def forward(self, x):
+        #The shape of input x should be (batch_size, input_size)
         output = self.fc(x)
         return output
